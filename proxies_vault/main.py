d094263bb7a4530d186d9856714037776e9d0753
thelacker <13740>
2020-04-21 16:09:31 +0300

    Перенес добавление эндпоинтов в отдельный файл

--- proxies_vault/main.py
+++ proxies_vault/main.py
@@ -1,20 +1,9 @@
 """
 Main launcher of app
 """
-from aiohttp import web
 
-from proxies_vault.routes import (get_ig, get_yt, post_ig_store, post_ig_update, post_proxy,
-                                  post_yt_store, post_yt_update)
+from server import Server
 
-APP = web.Application()
-APP.add_routes([web.get('/yt', get_yt),
-                web.get('/ig', get_ig),
-                web.post('/yt/store', post_yt_store),
-                web.post('/yt/update', post_yt_update),
-                web.post('/proxy', post_proxy),
-                web.post('/ig/store', post_ig_store),
-                web.post('/ig/update', post_ig_update)])
-
-if __name__ == "__main__":
-    print('ready')
-    web.run_app(APP)
+if __name__ == '__main__':
+    app = Server()
+    app.run()
--- proxies_vault/routes.py
+++ proxies_vault/routes.py
@@ -12,6 +12,14 @@ from proxies_vault.helpers import (get_free_ig_meta, get_free_yt_meta, store_ig_
 from proxies_vault.meta import (IgSessionStore, IgSessionUpdate, ProxyValid,
                                 YtApiKeyStore, YtApiKeyUpdate)
 
+def setup_routes(app):
+    app.router.add_get('/yt', get_yt, name='get_yt')
+    app.router.add_get('/ig', get_ig, name='get_ig')
+    app.router.add_post('/yt/store', post_yt_store, name='post_yt_store')
+    app.router.add_post('/yt/update', post_yt_update, name='post_yt_update')
+    app.router.add_post('/proxy', post_proxy, name='post_proxy')
+    app.router.add_post('/ig/store', post_ig_store, name='post_ig_store')
+    app.router.add_post('/ig/update', post_ig_update, name='post_ig_update')
 
 async def get_yt(request):
     """
